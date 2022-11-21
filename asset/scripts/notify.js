6dad8a9451517dfecff21c6de50e8ccebda5aea9
Dns Bsk <dev.tonerfish@seznam.cz>
2022-11-21 16:56:25 +0100

    notify

--- asset/scripts/notify.js
+++ asset/scripts/notify.js
@@ -1,2 +1,15 @@
-let notify = true;
-print(notify);
+const notify = true
+const audio = new Audio("asset/sounds/notify_sound.mp3")
+audio.play();
+
+if (notify == true){
+    setTimeout(notifyMe, 3000)
+}
+
+function notifyMe(){
+    audio.play();
+}
+
+console.log(notify)
+
+
